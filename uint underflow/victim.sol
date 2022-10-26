8a34d99a14e7addece93e478899b4629edd457ed
Raza <mraza6601@gmail.com>
2022-10-26 19:18:11 +0500

    uint underflow

---
+++ uint underflow/victim.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+contract Token {
+
+  mapping(address => uint) balances;
+  uint public totalSupply;
+
+  constructor(uint _initialSupply) public {
+    balances[msg.sender] = totalSupply = _initialSupply;
+  }
+
+  function transfer(address _to, uint _value) public returns (bool) {
+    require(balances[msg.sender] - _value >= 0);
+    balances[msg.sender] -= _value;
+    balances[_to] += _value;
+    return true;
+  }
+
+  function balanceOf(address _owner) public view returns (uint balance) {
+    return balances[_owner];
+  }
+}
+
+/*
+Vulnerability:
+
+Since the compiler version is <0.8 and no safe Math library is used
+we can exploit the uint underflow and overflow
+By calling the transfer with value > 20 , the msg.sender will have crazyyyyy amount of tokens
+
+*/
+
