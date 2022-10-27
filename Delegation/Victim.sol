da0e549d0fc689deb4752c86693f05d7ff29a07f
Raza <mraza6601@gmail.com>
2022-10-27 19:13:15 +0500

    delegation done

---
+++ Delegation/Victim.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+contract Delegate {
+
+  address public owner;
+
+  constructor(address _owner) public {
+    owner = _owner;
+  }
+
+  function pwn() public {
+    owner = msg.sender;
+  }
+}
+
+contract Delegation {
+
+  address public owner;
+  Delegate delegate;
+
+  constructor(address _delegateAddress) public {
+    delegate = Delegate(_delegateAddress);
+    owner = msg.sender;
+  }
+
+  fallback() external {
+    (bool result,) = address(delegate).delegatecall(msg.data);
+    if (result) {
+      this;
+    }
+  }
+}
+
+/*
+If we call the fallback function of the Delegation Contract,
+We  will be calling the delegatecall function
+if we pass the msg.data to be the signature of the pwn function 
+We will succesfully have the ownership of the contract
+
+
+
+*/
